818e6dab299ed85a0a141079e920e2e1c1e877cb
Piffek <patrykpiwko123412@gmail.com>
2017-04-28 14:09:33 +0200

    add connection to db and query builder

--- index.php
+++ index.php
@@ -5,20 +5,15 @@ use Database\QueryBuilder as QueryBuilder;
 
 require'src/bootstrap.php';
 
-
+/*
 $query = new QueryBuilder;
 foreach($query->selectAll() as $row)
 {
 	echo $row['id'];
 }
+*/
 
 
-//$router = new Router;
 
-//require 'routes.php';
-
-//require $router->redirect($urls);
-
-
-//require Router::load(__DIR__.'/routes.php')->redirect(Request::url(), Request::method());
+require Router::load(__DIR__.'/routes.php')->redirect(Request::url(), Request::method());
 
